4108919395228345c1257a0dac90584720f474dc
Isaac <isaac_carrillo@ucol.mx>
2022-07-18 23:14:17 -0500

    Se han agregado comentarios

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { dirname, join } from "path";
 import route from "./routes/index.js";
 
 const app = express();
+// PORT DEFAULT
 const PORT = 3000;
 
 app.use(route);
 
+// SHOW PORT SERVER
 app.listen(process.env.PORT || PORT, ()=> {
     console.log("SERVER IN PORT:", process.env.PORT || PORT);
 });
